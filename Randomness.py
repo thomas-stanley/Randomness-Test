0bb9fa1ab3686ef33fe41c2aaa8c4c80624bf641
Thomas Stanley <111903534+thomas-stanley@users.noreply.github.com>
2024-10-03 13:39:08 +0000

    More subroutine compartmentalisation and further improvements.

--- Randomness.py
+++ Randomness.py
@@ -1,4 +1,4 @@
-data = ""
+
 data_dict = {"000": "0,0",
              "001": "0,0",
              "010": "0,0",
@@ -8,29 +8,45 @@ data_dict = {"000": "0,0",
              "110": "0,0",
              "111": "0,0"}
 balance = 50
-print("Please provide the computer with some data... ")
-while len(data) < 100:
-    print(f"The current data length is {len(data)}, {100 - len(data)} symbols left.")
-    user_input = input("Print a random string containing 0 or 1:\n")
-    for x in user_input:
-        if x in "10":
-            data += x
 
-print(f"\nFinal data string:\n{data}\n")
-options = ["000", "001", "010", "011", "100", "101", "110", "111"]
-for i in range(len(data) - 3):
-    three_letters = f"{data[i]}{data[i + 1]}{data[i + 2]}"
-    values = data_dict[three_letters]
-    if data[i + 3] == "0":
-        data_dict[three_letters] = f"{int(values.split(',')[0]) + 1}, {values.split(',')[-1]}".replace(" ", "")
-    if data[i + 3] == "1":
-        data_dict[three_letters] = f"{values.split(',')[0]}, {int(values.split(',')[-1]) + 1}".replace(" ", "")
-    three_letters = ""
 
-print("You have 50 points. Every time the system successfully predicts your next press, you lose 1 point.\nOtherwise, you earn 1 point. Print 'exit' to leave the game. ")
+def user_data():
+    input_data = ""
+    print("Please provide the computer with some data... ")
+    # Takes in at least 100 digits of user's data in order to form the trigrams
+    while len(input_data) < 100:
+        print(f"The current data length is {len(input_data)}, {100 - len(input_data)} symbols left.")
+        user_input = input("Print a random string containing 0 or 1:\n")
+        for number in user_input:
+            # Ensures data is valid-
+            if number in "10":
+                input_data += number
+    print(f"\nFinal data string:\n{input_data}\n")
+    # options = ["000", "001", "010", "011", "100", "101", "110", "111"]
+    return input_data
+
+
+initial_data = user_data()
+
+
+def create_trigrams(data):
+    # Creates the trigrams for the frequency analysis
+    for i in range(len(data) - 3):
+        three_letters = f"{data[i]}{data[i + 1]}{data[i + 2]}"
+        values = data_dict[three_letters]
+        if data[i + 3] == "0":
+            data_dict[three_letters] = f"{int(values.split(',')[0]) + 1}, {values.split(',')[-1]}".replace(" ", "")
+        if data[i + 3] == "1":
+            data_dict[three_letters] = f"{values.split(',')[0]}, {int(values.split(',')[-1]) + 1}".replace(" ", "")
+
+
+create_trigrams(initial_data)
+print("You have 50 points. Every time the system successfully predicts your next press, you lose 1 point.\n"
+      "Otherwise, you earn 1 point. Print 'exit' to leave the game. ")
 
 while True:
     while True:
+        # Starts the game and does input sanitation
         score = 0
         prediction = ""
         flag = True
@@ -45,6 +61,7 @@ while True:
                     continue
             if flag:
                 break
+    # Creates a prediction
     for i in range(len(test_string) - 3):
         testing_triad = f"{test_string[i]}{test_string[i + 1]}{test_string[i + 2]}"
         value_1 = int(data_dict[testing_triad].split(',')[0])
@@ -54,12 +71,15 @@ while True:
         else:
             prediction += "1"
     print(f"predictions:\n{prediction}\n")
+    # Keeps score
     for i in range(len(prediction)):
         if prediction[i] == test_string[i + 3]:
             score += 1
     print(f"Computer guessed {score} out of {len(prediction)} symbols right ({round(score / len(prediction) * 100, 2)}%)")
     balance += len(prediction) - score
     balance -= score
+    # Checks if balance reaches 0
+    create_trigrams(test_string)
     if balance <= 0:
         print("Game over! You ran out of points.")
         exit()
